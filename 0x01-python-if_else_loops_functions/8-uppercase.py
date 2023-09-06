76192d2684be2ffad295728cef0d96d682032c30
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-09-07 02:28:30 +0300

    Making use of String formatter

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             upper = chr(ord(str[i]) - 97 + 65)
-            print(upper, end="" if i < len(str) - 1 else "\n")
+            print("{}".format(upper), end="" if i < len(str) - 1 else "\n")
         else:
-            print(str[i], end="" if i < len(str) - 1 else "\n")
+            print("{}".format(str[i]), end="" if i < len(str) - 1 else "\n")
