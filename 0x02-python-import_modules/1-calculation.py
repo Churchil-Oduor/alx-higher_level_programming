129aebfd0ea6829be3651b8ec622214a6df499ed
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-09-08 03:06:36 +0300

    Correction to include valid import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import calculator as calc
+    import calculator_1
 
     a = 10
     b = 5
 
-    my_sum = calc.add(a, b)
-    sub = calc.sub(a, b)
-    div = calc.div(a, b)
-    mul = calc.mul(a, b)
+    my_sum = calculator_1.add(a, b)
+    sub = calculator_1.sub(a, b)
+    div = calculator_1.div(a, b)
+    mul = calculator_1.mul(a, b)
 
     print("{} + {} = {}".format(a, b, my_sum))
     print("{} - {} = {}".format(a, b, sub))
