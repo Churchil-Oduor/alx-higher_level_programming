7047f1cfde57bb16bda4a5e62fad8df21cb98133
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-09-13 15:03:29 +0300

    correction to use the correct format of str.format() when printing integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@ def print_reversed_list_integer(my_list=[]):
     length = len(my_list) - 1
     while length != -1:
         element = my_list[length]
-        print("{}".format(element))
+        print("{:d}".format(element))
         length -= 1
