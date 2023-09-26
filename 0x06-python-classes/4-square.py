c9f334a8eeaabcf95d102952425b489780259aa4
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-09-26 22:04:19 +0300

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,6 +18,7 @@ class Square:
             raise ValueError('size must be >= 0')
         self.__size = size
 
+    @property
     def size(self):
         """Retrieve the data
 
@@ -25,13 +26,14 @@ class Square:
         data = self.__size
         return data
 
+    @size.setter
     def size(self, value):
         """setter function
 
         """
         if type(value) is not int:
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise TypeError('size must be >= 0')
         self.__size = value
 
