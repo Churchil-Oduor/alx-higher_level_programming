ea819c0fe12b8478868d0c9d0c2dd6a814ff4519
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-09-26 21:43:54 +0300

    Adding task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+class square defines a square
+
+"""
+class Square:
+    """Initializing the class square and doing checks on input
+
+    Args:
+        size (int): integer to be fed into the square.
+
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise TypeError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """
+        length is the length of the square.
+        """
+        length = self.__size
+        return (length * length)
