c7f33018ad909789f634d56c196ae341d5c148ed
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-09-26 21:15:37 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """The size attribute is private.
+
+    Args:
+        size: the size of the square.
+
+    """
+    def __init__(self, size):
+        self.__size = size
