79b6f043c34cebb059268744540ab760c06db0c2
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-10-11 18:58:59 +0300

    Correction to handle subclass check correctly

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
         class ; otherwise False.
     '''
 
-    return isinstance(obj, a_class)
+    return (type(obj) is not a_class) and issubclass(type(obj), a_class)
