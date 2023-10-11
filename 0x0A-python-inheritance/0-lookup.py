48c7083b76d15b42f1ef5ddce39d0edc34ef709b
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-10-11 12:51:27 +0300

    Documented the code correctly

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
+
+'''This module contains only the lookup function.
+'''
+
+
 def lookup(obj):
     '''
     Args:
         obj: is an object having attributes
         that are looked up by the function lookup.
 
+    Returns:
+        a list containing method and attribute names.
     '''
     return (dir(obj))
