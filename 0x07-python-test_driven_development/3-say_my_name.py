c39e31bbcf0a6d84bca24bbb227d95e7b90813c1
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-01-31 23:05:46 +0300

    Task 3 added

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a function that prints the
+name of a person"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This is the function that prints the name of a
+    Person.
+
+    Args:
+        first_name (str): 1st name of a person.
+        last_name (str): last name of a person.
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        message = "My name is {} {}"
+        print(message.format(first_name, last_name))
