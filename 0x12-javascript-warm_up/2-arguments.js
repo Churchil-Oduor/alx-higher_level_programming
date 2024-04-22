9ef1408087d10ea711d72af2f6381f01a96d7851
Churchil-Oduor <churchilokechoduor>
2024-04-22 17:04:19 +0300

    modified code to feature block statememts

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,13 @@
 
 const { argv } = require('node:process');
 
-// print process.argv
-argv.forEach((val, index) => {
-	console.log(`${index}: ${val}`);
-});
+//check to see if any argument is passed
+const num = argv.length
+
+if (num === 3){
+	console.log('Argument found')
+}else if (num > 3){
+	console.log('Arguments found')
+}else {
+	console.log('No argument')
+}
