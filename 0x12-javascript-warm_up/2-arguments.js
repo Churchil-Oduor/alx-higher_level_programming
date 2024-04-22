d8621cdcf539e1637226c8b81271b3af7c36dbac
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-04-22 17:08:11 +0300

    Correctiions

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,13 @@
 
 const { argv } = require('node:process');
 
-//check to see if any argument is passed
-const num = argv.length
+// check to see if any argument is passed
+const num = argv.length;
 
-if (num === 3){
-	console.log('Argument found')
-}else if (num > 3){
-	console.log('Arguments found')
-}else {
-	console.log('No argument')
+if (num === 3) {
+  console.log('Argument found');
+} else if (num > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
