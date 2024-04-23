71de10a93d8af9482b120b5d4511fba20b649e66
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-04-23 20:12:55 +0300

    task 11 coming in

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,15 +4,22 @@ const { argv } = require('node:process');
 
 const count = argv.length;
 let x;
-let greatest = 0;
+let firstG = 0;
+let secondG = 0;
 
 if (count <= 3) {
   console.log(0);
 } else {
-  for (x = 3; x < count + 3; x++) {
-    if (argv[x] > greatest) {
-      greatest = argv[x];
+  for (x = 2; x < count + 2; x++) {
+    if (argv[x] > firstG) {
+      firstG = argv[x];
     }
   }
-  console.log(greatest);
+  for (x = 2; x < count + 2; x++) {
+    if (argv[x] > secondG && argv[x] < firstG) {
+      secondG = argv[x];
+    }
+  }
+
+  console.log(secondG);
 }
