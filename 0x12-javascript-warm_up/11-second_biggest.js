7cf845bd0bbd71ecba06b0e748b7aa08f2c8c513
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-04-23 20:01:03 +0300

    File 11 coming in

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,18 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process')
+const { argv } = require('node:process');
 
+const count = argv.length;
+let x;
+let greatest = 0;
+
+if (count <= 3) {
+  console.log(0);
+} else {
+  for (x = 3; x < count + 3; x++) {
+    if (argv[x] > greatest) {
+      greatest = argv[x];
+    }
+  }
+  console.log(greatest);
+}
