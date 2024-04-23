640b56d5bcdba3638945b1c2e52aff96c1143e76
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-04-23 19:51:00 +0300

    Corrections on file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process')
-
+const { argv } = require('node:process');
 
 function add (a, b) {
-	return parseInt(a) + parseInt(b);
+  return parseInt(a) + parseInt(b);
 }
 
 if (argv.length < 3 || argv.length < 3 || isNaN(argv[2]) || isNaN(argv[3])) {
-	console.log('NaN');
-
+  console.log('NaN');
 } else {
-	let x = argv[2];
-	let y = argv[3];
-	console.log(add(x, y));
+  console.log(add(argv[2], argv[3]));
 }
-
