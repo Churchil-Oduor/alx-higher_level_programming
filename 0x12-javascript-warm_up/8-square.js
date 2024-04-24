372b36b6ed5fb37fe7e44df94e405b2a92b9a4b9
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-04-24 16:18:51 +0300

    corrections

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -8,9 +8,9 @@ if (length === 0 || isNaN(parseInt(argv[2]))) {
   console.log('Missing size');
 } else {
   let x, y;
-  let line = '';
 
   for (x = 0; x < argv[2]; x++) {
+    let line = '';
     for (y = 0; y < argv[2]; y++) {
       line += 'X';
     }
