983be30ec9d335a84d08294fd348a7796e03503a
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-04-30 16:19:16 +0300

    made corrections by adding GROUP BY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,4 @@
 -- script that lists the number of records with the same score in the table second_table
-SELECT score, name AS "number" FROM second_table;
+SELECT score, COUNT(name) AS 'number'
+FROM second_table
+GROUP BY score;
