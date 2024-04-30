cbeaca5c46710913bfefce3784e74b3ab19920de
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-04-30 16:11:49 +0300

    Correction so that output is arranged in descending order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- script that lists all records of the table second_table of the database htbn_0c _0
 -- in the mysql server.
 
-SELECT score, name FROM second_table;
+SELECT score, name FROM second_table ORDER BY score DESC;
